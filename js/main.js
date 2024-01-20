d766440c21bac725182b11a993348d67854ec392
sarahshmn <sara.sadat.hashemi.nezhad@gmail.com>
2024-01-20 17:07:53 +0330

    folders

---
+++ js/main.js
@@ -0,0 +1,218 @@
+const link = document.querySelector('.link');
+const boxes = document.querySelectorAll('.box');
+
+// Function to change opacity
+function changeOpacity() {
+  boxes.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link.addEventListener('mouseover', changeOpacity);
+link.addEventListener('mouseout', () => {
+  boxes.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+const link1 = document.querySelector('.link1');
+const boxes1 = document.querySelectorAll('.box1');
+
+// Function to change opacity
+function changeOpacity1() {
+  boxes1.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link1.addEventListener('mouseover', changeOpacity1);
+link1.addEventListener('mouseout', () => {
+  boxes1.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+
+const link2 = document.querySelector('.link2');
+const boxes2 = document.querySelectorAll('.box2');
+
+// Function to change opacity
+function changeOpacity2() {
+  boxes2.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link2.addEventListener('mouseover', changeOpacity2);
+link2.addEventListener('mouseout', () => {
+  boxes2.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+const link3 = document.querySelector('.link3');
+const boxes3 = document.querySelectorAll('.box3');
+
+// Function to change opacity
+function changeOpacity3() {
+  boxes3.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link3.addEventListener('mouseover', changeOpacity3);
+link3.addEventListener('mouseout', () => {
+  boxes3.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+const link4 = document.querySelector('.link4');
+const boxes4 = document.querySelectorAll('.box4');
+
+// Function to change opacity
+function changeOpacity4() {
+  boxes4.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link4.addEventListener('mouseover', changeOpacity4);
+link4.addEventListener('mouseout', () => {
+  boxes4.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+const link5 = document.querySelector('.link5');
+const boxes5 = document.querySelectorAll('.box5');
+
+// Function to change opacity
+function changeOpacity5() {
+  boxes5.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link5.addEventListener('mouseover', changeOpacity5);
+link5.addEventListener('mouseout', () => {
+  boxes5.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+const link6 = document.querySelector('.link6');
+const boxes6 = document.querySelectorAll('.box6');
+
+// Function to change opacity
+function changeOpacity6() {
+  boxes6.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link6.addEventListener('mouseover', changeOpacity6);
+link6.addEventListener('mouseout', () => {
+  boxes6.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+const link7 = document.querySelector('.link7');
+const boxes7 = document.querySelectorAll('.box7');
+
+// Function to change opacity
+function changeOpacity7() {
+  boxes7.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link7.addEventListener('mouseover', changeOpacity7);
+link7.addEventListener('mouseout', () => {
+  boxes7.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+
+const link8 = document.querySelector('.link8');
+const boxes8 = document.querySelectorAll('.box8');
+
+// Function to change opacity
+function changeOpacity8() {
+  boxes8.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link8.addEventListener('mouseover', changeOpacity8);
+link8.addEventListener('mouseout', () => {
+  boxes8.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+
+const link9 = document.querySelector('.link9');
+const boxes9 = document.querySelectorAll('.box9');
+
+// Function to change opacity
+function changeOpacity9() {
+  boxes9.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link9.addEventListener('mouseover', changeOpacity9);
+link9.addEventListener('mouseout', () => {
+  boxes9.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+const link10 = document.querySelector('.link10');
+const boxes10 = document.querySelectorAll('.box10');
+
+// Function to change opacity
+function changeOpacity10() {
+  boxes10.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link10.addEventListener('mouseover', changeOpacity10);
+link10.addEventListener('mouseout', () => {
+  boxes10.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
+
+const link11 = document.querySelector('.link11');
+const boxes11 = document.querySelectorAll('.box11');
+
+// Function to change opacity
+function changeOpacity11() {
+  boxes11.forEach(box => {
+    box.style.opacity = '1';
+  });
+}
+
+// Event listeners for hover
+link11.addEventListener('mouseover', changeOpacity11);
+link11.addEventListener('mouseout', () => {
+  boxes11.forEach(box => {
+    box.style.opacity = '0';
+  });
+});
